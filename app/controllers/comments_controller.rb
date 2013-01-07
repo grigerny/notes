91055e1a157806d55fdcebadeacebdb94700f136
Gary Riger <grigerny@aol.com>
2013-01-06 20:04:45 -0500

    made changes to comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,14 +2,6 @@ class CommentsController < ApplicationController
   def create
     @note = Note.find(params[:note_id]
     @comment = @note.comment.build(params[:comment])
-  
-  respond_to do |format|
-    if @comment.save
-      format.html { redirect_to root_url, notice: 'You added a comment.' }
-      format.json { render json: @comment, status: :created, location: @comment }
-    else
-      format.html { render action: "new" }
-      format.json { render json: @comment.errors, status: :unprocessable_entity }
-    end
+    @comment.save
   end    
 end
