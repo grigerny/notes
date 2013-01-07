9a39fb84ea0455ff02a180eae22bf44d2620ea0c
Gary Riger <grigerny@aol.com>
2013-01-06 22:46:32 -0500

    removed files

--- db/migrate/20130107020403_remove_note_id_from_comment.rb
+++
@@ -1,9 +0,0 @@
-class RemoveNoteIdFromComment < ActiveRecord::Migration
-  def up
-    change_column :comments, :note_id, :integer
-  end
-
-  def down
-    change_column :comments, :note_id, :string
-  end
-end
