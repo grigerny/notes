de0eeef0944301fbd7004b5cbd7d7c6c2d897f86
Gary Riger <grigerny@aol.com>
2013-01-06 22:57:33 -0500

    Added note_id integer to comments table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130107035212) do
+ActiveRecord::Schema.define(:version => 20130107035634) do
 
   create_table "comments", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.text     "text"
+    t.integer  "note_id"
   end
 
   create_table "notes", :force => true do |t|
