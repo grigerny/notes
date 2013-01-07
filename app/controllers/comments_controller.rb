a938090290a4a2dc09de12bf7c3d2e36ffbd60cc
Gary Riger <grigerny@aol.com>
2013-01-06 23:46:36 -0500

    major ui changes to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,16 +5,16 @@ class CommentsController < ApplicationController
 
       if @comment.save
         flash[:notice] = 'Comment was successfully created.'
-        redirect_to(@comment.note)
+        redirect_to(root_path)
       else
         flash[:notice] = "Error creating comment: #{@comment.errors}"
-        redirect_to(@comment.note)
+        redirect_to(root_path)
       end
     end
 
     def destroy
       @comment = Comment.find(params[:id])
       @comment.destroy
-      redirect_to(@comment.note)
+      redirect_to(root_path)
     end
 end
