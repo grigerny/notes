51557fa06907879ac6ccfc0c5ad888b52c7ec5f1
Gary Riger <grigerny@aol.com>
2013-01-22 21:31:36 -0500

    some refactoring

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,2 +1,11 @@
-module NotesHelper
+module NotesHelper 
+  
+  def truncate_link(note)
+  truncate("#{note.link.sub(/^https?\:\/\//, '').sub(/^www./,'')}", :length => 17, :separator => ' ') 
+  end
+  
+  def created_at_time(note)
+    note.created_at.strftime('%b %d, %Y') 
+  end
+  
 end
